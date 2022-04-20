cbdda17b743f767bc014d087f2771658e1de01e1
Prelipcean Cristian <prelipceancristian100@gmail.com>
2022-04-20 09:04:48 +0300

    add components

---
+++ Library/library/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <v-app style="background-color: rgba(46,64,87);">
+    <v-app-bar app dark :color="constants.mainBlue">
+      <v-btn icon @click="isNavigationOpen = !isNavigationOpen">
+        <v-icon>mdi-menu</v-icon>
+      </v-btn>
+      <h1>Library-online</h1>
+      <v-spacer></v-spacer>
+      <User :user="testUser" v-if="isUserLoggedIn"></User>
+    </v-app-bar>
+    <v-navigation-drawer
+      app
+      v-model="isNavigationOpen"
+      width="300"
+      dark
+      temporary
+      :color="constants.black"
+    >
+      <NavigationDrawerContent />
+    </v-navigation-drawer>
+    <v-main>
+      <Tiles v-if="isUserLoggedIn" />
+      <LoginScreen v-else />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import Tiles from './components/Tiles'
+import LoginScreen from './components/LoginScreen'
+import constants from './common/constants'
+import User from './components/User'
+import NavigationDrawerContent from './components/NavigationDrawerContent'
+
+export default {
+  name: 'App',
+
+  components: {
+    Tiles,
+    User,
+    NavigationDrawerContent,
+    LoginScreen
+  },
+
+  data: () => ({
+    isNavigationOpen: false,
+    constants: constants,
+    testUser: {
+      imageURL: '',
+      name: 'Prelipcean Cristian'
+    },
+    isUserLoggedIn: false
+  })
+}
+</script>
---
+++ Library/library/src/axios/requests.js
@@ -0,0 +1,23 @@
+import constants from '../common/constants'
+import Vue from 'vue'
+
+function request (type, endpoint, payload = null) {
+  switch (type) {
+    case constants.request_types.GET:
+      return Vue.axios.get(`${constants.server_api_url}${endpoint}`, payload)
+    case constants.request_types.POST:
+      return Vue.axios.post(`${constants.server_api_url}${endpoint}`, payload)
+    case constants.request_types.PUT:
+      return Vue.axios.put(`${constants.server_api_url}${endpoint}`, payload)
+    case constants.request_types.DELETE:
+      return Vue.axios.delete(`${constants.server_api_url}${endpoint}`, payload)
+  }
+}
+
+export function loginUser (payload) {
+  return request(
+    constants.request_types.GET,
+    constants.endpoints.users,
+    payload
+  )
+}
